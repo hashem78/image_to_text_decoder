2c39ee5234ba4e4a6b2d5fb072e2a771828e6a62
hashem78 <hashem.olayano@gmail.com>
2021-08-10 15:53:04 +0300

    Add clipboard_getter

---
+++ clipboard_getter/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <clipboard_getter/clipboard_getter_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ClipboardGetterPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ClipboardGetterPlugin"));
+}
